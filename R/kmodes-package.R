1a97e9c785c8a4b93b790179a00700248f51ced3
kdorman <kdorman@iastate.edu>
2021-06-04 13:15:13 -0500

    - not sure why this file is useful when we have DESCRIPTION

---
+++ R/kmodes-package.R
@@ -0,0 +1,13 @@
+#' @details
+#' The package kmodes provides only one function [kmodes()], which performs
+#' k-modes clustering on categorical datasets.
+#' @docType package
+#' @aliases kmodes-package
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
